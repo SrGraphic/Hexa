5bea3108f038480b3276970446d957990110af36
Srgraphic <sergiorsaweb18@gmail.com>
2026-02-08 12:08:48 +0100

    Update

--- process/app-exit.js
+++ process/app-exit.js
@@ -1,8 +1,8 @@
-// ====================== IMPORTAR FIREBASE ======================
+// BOTÓN SALIR //
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
-// ====================== CONFIGURACIÓN DEL PROYECTO ======================
+// Firebase-config
 const firebaseConfig = {
   apiKey: "AIzaSyBMbEupVHZXduK7Kixt7l_FbDHPAPYNTdI",
   authDomain: "hexa-304d9.firebaseapp.com",
@@ -12,30 +12,26 @@ const firebaseConfig = {
   appId: "1:919569150365:web:c47e58b72d3b65916c7f74"
 };
 
-// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// ====================== BOTONES DE SALIDA ======================
-
-// Mostrar confirmación de salida
+// modal-salida
 document.getElementById('salir-btn').addEventListener('click', () => {
     document.getElementById('confirmacion-salida').style.display = 'flex';
 });
 
-// Confirmar cierre de sesión
+//caso-cerrar-sesion
 document.getElementById('cerrar-btn').addEventListener('click', async () => {
     try {
-        await signOut(auth);  // 🔑 Cerrar sesión en Firebase
-        window.location.href = '../index.html'; // Redirigir al login
+        await signOut(auth); // confirmar-cierra-firebase
+        window.location.href = '../index.html';
     } catch (error) {
         console.error("Error al cerrar sesión:", error);
         alert("No se pudo cerrar sesión. Inténtalo de nuevo.");
     }
 });
 
-// Mantener sesión
+// caso-mantener-sesion
 document.getElementById('mantener-btn').addEventListener('click', () => {
     document.getElementById('confirmacion-salida').style.display = 'none';
 });
-
